27b345269c904e2a3bd0d6be62482bc73db1ddcf
Maziyar Panahi <maziyar@live.co.uk>
2026-07-17 13:34:03 +0200

    feat: add TIMEX3 temporal normalization

--- openmed/clinical/__init__.py
+++ openmed/clinical/__init__.py
@@ -199,6 +199,7 @@ from .summary_card import (
     ClinicalSummaryCard,
     build_summary_card,
 )
+from .temporal_normalizer import NormalizedTimex, normalize_temporal
 from .timeline import (
     TIMELINE_ASSISTIVE_DISCLAIMER,
     NormalizedInterval,
@@ -332,6 +333,8 @@ __all__ = [
     "ClinicalSummaryCard",
     "SUMMARY_CARD_NOTE",
     "build_summary_card",
+    "NormalizedTimex",
+    "normalize_temporal",
     "TemporalExpression",
     "detect_timexes",
     "NormalizedInterval",
