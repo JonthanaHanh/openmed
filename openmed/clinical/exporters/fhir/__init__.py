4b341c234d17033f7b40afa5381c2232421979ea
Maziyar Panahi <maziyar@live.co.uk>
2026-07-18 17:23:21 +0200

    feat: check SMART FHIR profile conformance

--- openmed/clinical/exporters/fhir/__init__.py
+++ openmed/clinical/exporters/fhir/__init__.py
@@ -8,12 +8,14 @@ from .operation_outcome import (
     from_validation_result,
     to_operation_outcome,
 )
+from .profile_check import check_bundle
 from .provenance import to_audit_event, to_provenance
 from .references import deterministic_fullurl
 
 __all__ = [
     "deterministic_fullurl",
     "OperationOutcomeIssue",
+    "check_bundle",
     "to_audit_event",
     "from_validation_result",
     "to_bundle",
